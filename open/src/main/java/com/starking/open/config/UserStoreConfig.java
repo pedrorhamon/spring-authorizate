5d6134f7ca7951166492e7694423caf407143f69
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-08-01 20:20:36 -0300

    Concluindo o UserDetail

---
+++ open/src/main/java/com/starking/open/config/UserStoreConfig.java
@@ -0,0 +1,27 @@
+package com.starking.open.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+/**
+ * @author pedroRhamon
+ */
+@Configuration
+public class UserStoreConfig {
+
+	@Bean
+	public UserDetailsService userDetailsService() {
+		var userDetailsManager = new InMemoryUserDetailsManager();
+		
+		userDetailsManager.createUser(
+				User.withUsername("admin")
+				.password("{noop}password")
+				.roles("USER")
+				.build());
+		
+		return userDetailsManager;
+	}
+}
