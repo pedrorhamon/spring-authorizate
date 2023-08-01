9a94cea17f4a6856bb24ac89ec6ea0852de44537
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-08-01 20:16:28 -0300

    Criando class para auth iniciando

---
+++ open/src/main/java/com/starking/open/config/SecurityFilterConfig.java
@@ -0,0 +1,44 @@
+package com.starking.open.config;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
+
+/**
+ * @author pedroRhamon
+ */
+
+@Configuration
+public class SecurityFilterConfig {
+	
+	@Bean
+	public SecurityFilterChain authSecurityFilterChain(HttpSecurity http) throws Exception {
+		OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
+		
+		http.getConfigurer(OAuth2AuthorizationServerConfigurer.class)
+		.oidc(
+				withDefaults())
+		.and()
+		.exceptionHandling((exceptions) -> exceptions.authenticationEntryPoint(
+				new LoginUrlAuthenticationEntryPoint("/login")))
+		.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+		
+		return http.build();
+	}
+	
+	@Bean
+	public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests((authorize) -> authorize.anyRequest()
+				.authenticated())
+		.formLogin(withDefaults());
+		
+		return http.build();
+	}
+}
