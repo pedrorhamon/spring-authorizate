4bde7f6ee8752288159c374265c460d6961e51a1
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-08-08 21:34:02 -0300

    concluindo configuração de client e security

--- client/src/main/java/com/starking/client/ClientController.java
+++ client/src/main/java/com/starking/client/ClientController.java
@@ -1,7 +1,9 @@
 package com.starking.client;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,12 +17,17 @@ public class ClientController {
 
 	@GetMapping("home")
 	public Mono<String> home(
-			@RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient client) {
+			@RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient client, 
+			@AuthenticationPrincipal OidcUser oidcUser) {
 		return Mono.just("""
 				<h2>Acess Token: %s</h2>
 				<h2>Refresh Token: %s</h2>
 				<h2>Id Token: %s</h2>
 				<h2>Claims: %s</h2>
-				""".formatted(client.getAccessToken().getTokenValue()));
+				""".formatted(
+						client.getAccessToken().getTokenValue(), 
+						client.getRefreshToken().getTokenValue(),
+						oidcUser.getIdToken().getTokenValue(),
+						oidcUser.getClaims()));
 	}
 }
